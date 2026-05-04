b89855d190bef266c57db95a62526d6790059fa0
Your Name <you@example.com>
2026-05-04 03:01:48 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -221,12 +221,36 @@ void eraseTreeMap(TreeMap * tree, void* key){
 // Pair* nextTreeMap(TreeMap* tree) retornar el siguiente Pair del mapa a partir del puntero TreeNode* current. 
 // Recuerde actualizar este puntero.
 
-Pair * firstTreeMap(TreeMap * tree) {
-    return NULL;
+Pair * firstTreeMap(TreeMap * tree)
+{
+    if (tree->root == NULL) return NULL;
+    
+    TreeNode *node = minimum(tree->root);
+    tree->current = node;
+    return node->pair;
 }
 
-Pair * nextTreeMap(TreeMap * tree) {
-    return NULL;
+Pair * nextTreeMap(TreeMap * tree)
+{
+    if (tree->current == NULL) return NULL;
+
+    TreeNode *node = tree->current;
+
+    if (node->right != NULL)
+    {
+        node = minimum(node->right);
+        tree->current = node;
+        return node->pair;
+    }
+
+    while (node->parent != NULL && node == node->parent->right)
+    {
+        node = node->parent;
+    }
+
+    tree->current = node->parent;
+    if (tree->current == NULL) return NULL;
+    return tree->current->pair;
 }
 
 // 7. La función Pair* upperBound(TreeMap* tree, void* key) retorna el Pair con clave igual a key. 
